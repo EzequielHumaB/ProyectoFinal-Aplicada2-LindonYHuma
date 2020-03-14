38c1abcf56cd8a0a5c765df9324c9a5a510cab20
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-03-14 19:08:36 -0400

    Agregando las propiedades del contexto

--- Aplicada2-ProyectoFinal/Data/Contexto.cs
+++ Aplicada2-ProyectoFinal/Data/Contexto.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Aplicada2_ProyectoFinal.Models;
 
 namespace Aplicada2_ProyectoFinal.Data
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlite("Data Source = Database/ProyectoFinalDatabaseHumaLindon.db"));
+        }
+
+        public DbSet<Productos> Productos { get; set; }
+        public DbSet<Proveedores> Proveedores { get; set; }
+        public DbSet<Compras> Compras { get; set; }
+        public DbSet<Ventas> Ventas { get; set; }
+        public DbSet<Pagos> Pagos { get; set; }
+        public DbSet<Usuarios> Usuarios { get; set; }
     }
 }
