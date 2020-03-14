6453fc88097432cf3a1e23b5ee0de9402c0b09dd
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2020-03-14 19:01:18 -0400

    sqlite instalado

--- Aplicada2-ProyectoFinal/Models/VentasDetalle.cs
+++ Aplicada2-ProyectoFinal/Models/VentasDetalle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,24 @@ namespace Aplicada2_ProyectoFinal.Models
 {
     public class VentasDetalle
     {
+        [Key]
+        public int Id { get; set; }
+        public int VentaId { get; set; }
+        public int ProductoId { get; set; }
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioVenta { get; set; }
+        public decimal SubTotal { get; set; }
 
+        public VentasDetalle()
+        {
+            Id = 0;
+            VentaId = 0;
+            ProductoId = 0;
+            Nombre = string.Empty;
+            Cantidad = 0;
+            PrecioVenta = 0;
+            SubTotal = 0;
+        }
     }
 }
