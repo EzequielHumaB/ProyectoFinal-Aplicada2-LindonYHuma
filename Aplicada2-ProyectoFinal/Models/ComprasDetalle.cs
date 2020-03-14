1a07ef904193bf5a6687a54255c7fa2c63aab2d7
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2020-03-14 19:06:48 -0400

    Toda la grasa hecha

--- Aplicada2-ProyectoFinal/Models/ComprasDetalle.cs
+++ Aplicada2-ProyectoFinal/Models/ComprasDetalle.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,24 @@ namespace Aplicada2_ProyectoFinal.Models
 {
     public class ComprasDetalle
     {
+        [Key]
+        public int Id { get; set; }
+        public int CompraId { get; set; }
+        public int ProductoId { get; set; }
+        public string Nombre { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioCompra { get; set; }
+        public decimal SubTotal { get; set; }
+
+        public ComprasDetalle()
+        {
+            Id = 0;
+            CompraId = 0;
+            Nombre = string.Empty;
+            ProductoId = 0;
+            Cantidad = 0;
+            PrecioCompra = 0;
+            SubTotal = 0;
+        }
     }
 }
--- Aplicada2-ProyectoFinal/Models/Ventas.cs
+++ Aplicada2-ProyectoFinal/Models/Ventas.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +9,25 @@ namespace Aplicada2_ProyectoFinal.Models
 {
     public class Ventas
     {
+        [Key]
+        [Required(ErrorMessage = "El VentaId debe ser un numero")]
+        [Range(0, 100000000, ErrorMessage = "El campo Id no puede ser menor que cero")]
+        public int VentaId { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required(ErrorMessage = "El campo fecha no puede estar vacío")]
+        public DateTime FechaVenta { get; set; }
+        public decimal Total { get; set; }
 
+        [ForeignKey("VentaId")]
+        public virtual List<VentasDetalle> VentasDetalles { get; set; }
+
+        public Ventas()
+        {
+            VentaId = 0;
+            FechaVenta = DateTime.Now;
+            Total = 0;
+
+            VentasDetalles = new List<VentasDetalle>();
+        }
     }
 }
